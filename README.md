0e3b6a8400b75bc88bc55be7ae02262e442cfc1d
tannerdsilva <t@tannersilva.com>
2024-06-04 19:35:09 -0500

    in the beginning...

---
+++ README.md
@@ -0,0 +1,1 @@
+With God as our witness, the essence of freedom, privacy, and security are eternally encrypted, discrete, and distributed.
---
+++ Sources/main.swift
@@ -0,0 +1,3 @@
+// (c) tanner silva 2024. all rights reserved.
+
+print("in pursuit of a better tomorrow, project hacklash has broken ground. hello world!")
