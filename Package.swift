542015c8c0f2275cf19a7aa951aec55ec0906ef0
tannerdsilva <t@tannersilva.com>
2024-06-22 13:30:53 -0500

    preparing to develop wireguard userspace

--- Package.swift
+++ Package.swift
@@ -5,10 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "hacklash",
+	platforms:[
+		.macOS(.v12)
+	],
+	products: [
+		.library(name:"wireguard-userspace", targets:["wireguard-userspace"]),
+	],
+	dependencies: [
+		.package(url:"https://github.com/tannerdsilva/rawdog.git", branch:"hacklash"),
+		.package(url:"https://github.com/apple/swift-nio.git", "2.50.0"..<"3.0.0"),
+	],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "hacklash"),
+		.target(
+			name:"wireguard-userspace",
+			dependencies: [
+				.product(name:"RAW", package:"rawdog"),
+				.product(name:"NIO", package:"swift-nio"),
+			]
+		),
     ]
 )
--- Sources/main.swift
+++
@@ -1,3 +0,0 @@
-// (c) tanner silva 2024. all rights reserved.
-
-print("in pursuit of a better tomorrow, project hacklash has broken ground. hello world!")
